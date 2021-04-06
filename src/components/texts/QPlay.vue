379068e3cddb442999502e0cfb18060c96e4a2aa
Mia <vottus@vott.us>
2021-04-06 17:40:24 +0200

    feat: logos

---
+++ src/components/texts/QPlay.vue
@@ -0,0 +1,10 @@
+<template>
+  <span class="flex items-center">
+    Developer at
+    <img
+      src="../../images/qp.jpg"
+      alt="TypeScript logo"
+      class="mx-2 w-8 h-8 rounded"
+    />
+  </span>
+</template>
